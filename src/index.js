b372e3aa23d811266d69c4cde538ceebab7207a3
Vine Brancho <vinebrancho@gmail.com>
2017-08-01 18:01:11 +0900

    remove unencessary css file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from 'store/configureStore';
 import App from 'components/App';
-import './index.css';
 
 let store = configureStore();
 
