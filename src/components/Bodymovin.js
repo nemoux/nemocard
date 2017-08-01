2b555118ddfa205d4cd060b062bfb345d5002b4b
haruband <haruband@harubands-MacBook-Pro.local>
2017-08-01 17:38:36 +0900

    rename Bodymovin component to Motion component

--- src/components/Bodymovin.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Bodymovin = ({meta}) => {
-  return (
-    <div {...meta}></div>
-  );
-};
-
-export default Bodymovin; 
---
+++ src/components/Motion.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Bodymovin from 'bodymovin';
+
+const Motion = ({meta}) => {
+  return (
+    <div {...meta}></div>
+  );
+};
+
+export default Motion;
