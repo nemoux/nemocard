33f672a7d866f124bb17a3c0b95f360d282fc615
haruband <haruband@gmail.com>
2017-08-08 17:42:23 +0900

    add nemobus methods

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from 'store/configureStore';
 import App from 'components/App';
+import * as nemobusMethods from 'libs/nemobus-methods';
+
 if (process.env.ELECTRON) {
   const { disablePinchZoom } = require('electron/renderer');
   disablePinchZoom();
+}
+
+if (process.env.ELECTRON) {
+	nemobusMethods.connect('/tmp/nemo.bus.0');
+	nemobusMethods.advertise('set', '/nemocard');
 }
 
 let store = configureStore();
