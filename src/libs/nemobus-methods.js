ad32a096528e159d80e4040b3ec004b53969fb45
haruband <haruband@gmail.com>
2017-08-08 17:42:23 +0900

    add nemobus methods

---
+++ src/libs/nemobus-methods.js
@@ -0,0 +1,56 @@
+const net = require('net');
+
+var client;
+
+export function connect(domain) {
+	client = net.connect(domain);
+}
+
+export function advertise(type, path) {
+	var msg = {};
+	
+	msg.to = '/nemobusd';
+	msg.advertise = {};
+	msg.advertise.type = type;
+	msg.advertise.path = path;
+	
+	client.write(JSON.stringify(msg));
+}
+
+export function execute(action, x, y) {
+	var msg = {};
+	
+	msg.from = '/nemocard';
+	msg.to = '/nemoshell';
+	msg.command = {};
+	msg.command.type = action.type;
+	msg.command.path = action.path;
+	msg.command.args = '';
+	msg.command.envp = '';
+	msg.command.states = '';
+	
+	for (var key in action.param) {
+		if (key === '#optind') {
+			msg.command.args += action.param[key] + ';';
+		} else if (action.param[key] === '#novalue') {
+			msg.command.args += '--' + key + ';';
+		} else {
+			msg.command.args += '--' + key + ';' + action.param[key] + ';';
+		}
+	}
+
+	for (var key in action.environ) {
+		msg.command.envp += key + '=' + action.environ[key] + ';';
+	}
+	
+	msg.command.states += 'x;' + x + ';';
+	msg.command.states += 'y;' + y + ';';
+	
+	for (var key in action.state) {
+		msg.command.states += key + ';' + action.state[key] + ';';
+	}
+
+	console.log('msg: ', msg);
+	
+	client.write(JSON.stringify(msg));
+}
